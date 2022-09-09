7845239cfd3b08da831f17694062a172ae4f5b80
Sergio Falcón <sergio@oniric.es>
2022-09-09 09:34:45 +0200

    Añadido Serviceprovider

---
+++ src/sp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sergiofalcon\Thumbnailer\Classes;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThumbnailerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+	    //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+	    //
+    }
+}
